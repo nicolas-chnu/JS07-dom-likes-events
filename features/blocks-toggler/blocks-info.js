168b3b619893e9e5c9adce9e97eb98bcf4b4b6ec
Nicolas <niiccoollaass442@gmail.com>
2024-04-18 19:28:54 +0300

    Implement block info rendering

---
+++ features/blocks-toggler/blocks-info.js
@@ -0,0 +1,24 @@
+export function renderBlocksInfo(destElem, blocksParent) {
+    let i = 1;
+
+    blocksParent.childNodes.forEach(b => {
+        const containerTop = b.offsetTop - blocksParent.offsetTop;
+        const containerLeft = b.offsetLeft - blocksParent.offsetLeft;
+        const containerScrollTop = Math.floor(containerTop - blocksParent.scrollTop);
+
+        destElem.innerHTML += `
+        <article class="block-info">
+            <h3 class="block-info__name">Block ${i}</h3>
+            <section class="block-info__attrs">
+                 <p class="block-attr">width: ${b.clientWidth}px</p>       
+                 <p class="block-attr">height: ${b.clientHeight}px</p>
+                 <p class="block-attr">window offset X: ${b.offsetLeft}px</p>
+                 <p class="block-attr">window offset Y: ${b.offsetTop}px</p>
+                 <p class="block-attr">parent offset X: ${containerLeft}px</p>
+                 <p class="block-attr">parent offset Y: ${containerTop}px</p>
+                 <p class="block-attr">current scroll offset Y: ${containerScrollTop}px</p>
+            </section>
+        </article>`
+        i++;
+    });
+}
--- features/blocks-toggler/main.js
+++ features/blocks-toggler/main.js
@@ -1,8 +1,17 @@
 import {renderNonToggleBlocks} from "./blocks-rendering.js";
+import {renderBlocksInfo} from "./blocks-info.js";
 
 const BLOCKS_COUNT = 12;
 
 document.addEventListener('DOMContentLoaded', () => {
     const blockList = document.querySelector('.blocks-list');
     renderNonToggleBlocks(blockList, BLOCKS_COUNT);
+
+    const blocksInfoElem = document.querySelector('.blocks-info');
+
+    document.querySelector('.get-blocks-info-btn')
+        .addEventListener('click', () => {
+            blocksInfoElem.innerHTML = '';
+            renderBlocksInfo(blocksInfoElem, blockList);
+        });
 });
