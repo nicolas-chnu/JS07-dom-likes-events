887f7c15eb64990dfcd6a52e7f98e9320de17180
Nicolas <niiccoollaass442@gmail.com>
2024-04-18 22:34:59 +0300

    Refactor blocks-info.js

--- features/blocks-toggler/blocks-info.js
+++ features/blocks-toggler/blocks-info.js
@@ -3,23 +3,41 @@ export function renderBlocksInfo(destElem, blocksParent) {
 
     blocksParent.childNodes.forEach(b => {
         const blockId = b.getAttribute('id');
-        const containerTop = b.offsetTop - blocksParent.offsetTop;
-        const containerLeft = b.offsetLeft - blocksParent.offsetLeft;
-        const containerScrollTop = Math.floor(containerTop - blocksParent.scrollTop);
+        const blockInfo = createBlockInfo(`Block ${i}`, blockId);
 
-        destElem.innerHTML += `
-        <article class="block-info" data-block-id="${blockId}">
-            <h3 class="block-info__name">Block ${i}</h3>
-            <section class="block-info__attrs" style="border-color: ${b.style.backgroundColor}">
-                 <p class="block-attr">width: ${b.clientWidth}px</p>       
-                 <p class="block-attr">height: ${b.clientHeight}px</p>
-                 <p class="block-attr">window offset X: ${b.offsetLeft}px</p>
-                 <p class="block-attr">window offset Y: ${b.offsetTop}px</p>
-                 <p class="block-attr">parent offset X: ${containerLeft}px</p>
-                 <p class="block-attr">parent offset Y: ${containerTop}px</p>
-                 <p class="block-attr">scroll offset Y: ${containerScrollTop}px</p>
-            </section>
-        </article>`
+        destElem.appendChild(blockInfo);
+        updateBlockInfoAttrs(b, blockInfo);
         i++;
     });
+}
+
+export function createBlockInfo(title, blockId) {
+    const elem = document.createElement('article');
+    elem.setAttribute('data-block-id', blockId);
+    elem.classList.add('block-info');
+
+    elem.innerHTML = `
+    <h3 class="block-info__name">${title}</h3>
+    <section class="block-info__attrs"></section>`;
+
+    return elem;
+}
+
+export function updateBlockInfoAttrs(block, blockInfo) {
+    const attrList = blockInfo.querySelector('.block-info__attrs');
+    attrList.style.borderColor = block.style.backgroundColor;
+
+    const blocksParent = block.parentElement;
+    const containerTop = block.offsetTop - blocksParent.offsetTop;
+    const containerLeft = block.offsetLeft - blocksParent.offsetLeft;
+    const containerScrollTop = Math.floor(containerTop - blocksParent.scrollTop);
+
+    attrList.innerHTML = `
+    <p class="block-attr">width: ${block.clientWidth}px</p>       
+             <p class="block-attr">height: ${block.clientHeight}px</p>
+             <p class="block-attr">window offset X: ${block.offsetLeft}px</p>
+             <p class="block-attr">window offset Y: ${block.offsetTop}px</p>
+             <p class="block-attr">parent offset X: ${containerLeft}px</p>
+             <p class="block-attr">parent offset Y: ${containerTop}px</p>
+             <p class="block-attr">scroll offset Y: ${containerScrollTop}px</p>`;
 }
