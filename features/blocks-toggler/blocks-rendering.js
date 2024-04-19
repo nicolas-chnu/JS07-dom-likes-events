30e81011daf247bf28bb0be518ea248d5b6cc382
Nicolas <niiccoollaass442@gmail.com>
2024-04-19 16:06:59 +0300

    Create a BlockFactory abstraction

--- features/blocks-toggler/blocks-rendering.js
+++ features/blocks-toggler/blocks-rendering.js
@@ -1,20 +1,31 @@
 let blockCounter = 1;
+const blockFactory = new BlockFactory();
 
 export function fillWithNonToggleBlocks(blocksParent, count) {
     blocksParent.innerHTML = '';
-    blockCounter = 0;
+    blockFactory.resetCount()
 
-    while (blockCounter < count) {
-        const block = document.createElement('div');
-        block.setAttribute('id', `block-${blockCounter + 1}`);
-        block.classList.add('non-toggle-block');
+    while (blockFactory.blocksCount < count) {
+        const block = blockFactory.createNonToggleBlock();
 
         applyRandomBg(block);
         applyRandomSize(block, 50, 150);
 
         blocksParent.appendChild(block);
-        blockCounter++;
     }
+}
+
+function applyRandomSize(elem, minPx, maxPx) {
+    elem.style.height = randomPx(minPx, maxPx + 1);
+    elem.style.width = randomPx(minPx, maxPx + 1);
+}
+
+function applyRandomBg(elem) {
+    elem.style.backgroundColor = getRandomColor();
+}
+
+function randomPx(lower, upper) {
+    return Math.floor(Math.random() * (upper - lower)) + lower + 'px';
 }
 
 function applyRandomSize(elem, minPx, maxPx) {
@@ -38,4 +49,27 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+class BlockFactory {
+    blocksCount = 0;
+
+    createNonToggleBlock() {
+        const block = this.#createBlock();
+        block.classList.add('non-toggle-block');
+
+        return block;
+    }
+
+    resetCount() {
+        this.blocksCount = 0;
+    }
+
+    #createBlock() {
+        const block = document.createElement('div');
+        block.setAttribute('id', `block-${this.blocksCount + 1}`);
+        this.blocksCount++;
+
+        return block;
+    }
 }
