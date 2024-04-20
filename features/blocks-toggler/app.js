83e822de41a35f05fb5e3f20b5b1ccc520cfde69
Nicolas <niiccoollaass442@gmail.com>
2024-04-20 16:52:37 +0300

    Add action handling interface

--- features/blocks-toggler/app.js
+++ features/blocks-toggler/app.js
@@ -1,14 +1,19 @@
-import {refreshBlockList} from "./block-list.js";
-import {} from "./block-info-list.js";
+import {addTogglerBlock, refreshBlockList} from "./block-list.js";
+import {generateBlockInfoList, updateBlockInfoList} from "./block-info-list.js";
 import {} from "./toggle-list.js";
 
 const INITIAL_BLOCK_COUNT = 12;
 
+let actionsButtons;
+let blockList;
+let blockInfoList;
+let toggleList;
+
 document.addEventListener('DOMContentLoaded', () => {
-    const actionsButtons = document.querySelector('.actions');
-    const blockList = document.querySelector('.blocks-list');
-    const blockInfoList = document.querySelector('.blocks-info');
-    const toggleList = document.querySelector('.toggle-stack');
+    actionsButtons = document.querySelector('.actions');
+    blockList = document.querySelector('.blocks-list');
+    blockInfoList = document.querySelector('.blocks-info');
+    toggleList = document.querySelector('.toggle-stack');
 
     actionsButtons.addEventListener('click', e => handleAction(e));
     blockList.addEventListener('click', e => handleBlockClick(e));
@@ -17,7 +22,27 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function handleAction(event) {
-    return undefined;
+    const action = event.target.hasAttribute('data-action');
+
+    if (!action) {
+        return;
+    }
+
+    switch (action) {
+        case "get-blocks-info":
+            generateBlockInfoList(blockInfoList, blockList.children);
+            break;
+        case "add-toggle-block":
+            addTogglerBlock(blockList);
+            break;
+        case "refresh-block-list":
+            refreshBlockList(blockList);
+
+            if (blockInfoList.innerHTML !== '') {
+                updateBlockInfoList(blockInfoList, blockList.children);
+            }
+            break;
+    }
 }
 
 function handleBlockClick(event) {
